c829126866c4f3291f2506151d2b62057c7e0e64
Derek Reese <cyb.tachyon@gmail.com>
2020-10-28 23:01:27 -0700

    WIP

---
+++ web/themes/custom/my_theme/js/my-theme.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * My Theme behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.myTheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
